27ca19b60b9075769bf8c77a8afb9b2e60b44c9a
Bencodev <benoit.cotte@essec.edu>
2017-03-25 21:32:45 +0100

    clean github integration and simplify objects

--- github_integration.py
+++ github_integration.py
@@ -10,24 +10,39 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 
 
+ITEM_LIST_MATCHING = {
+	"issue": "issues",
+	"milestone": "milestones"
+}
+
+
 class Github(GithubApi):
+
 	def __init__(self, username, pwd, repo_name=None, user=None):
 		logging.info("Github - Init")
+
 		super(Github, self).__init__(username, pwd)
+
 		self.repo_name = repo_name
-
-		self.milestones = []
-		self.single_issues = []
-		self.load()
-
+		self.milestones = set()
+		self.issues = set()
 		self.user = user
 
 	def load(self):
+		"""
+		Iterate over Github issues
+
+		Append data in self.milestones if is a issue has a milestone
+		Append data in self.issues
+
+		FIXME: Limit: using list to store the issues, should use generator and fetch one by one and process one by one
+		:return:
+		"""
 		logging.info("Github - Loading milestones")
 
 		issues = self.get_repo(self.repo_name).get_issues(state="all")
 
-		total_count = issues.totalCount or len(list(issues))
+		total_count = len(list(issues)) or issues.totalCount
 
 		for index, issue in enumerate(issues):
 			logging.info("Github - Loading issue {} / {} ".format(index + 1, total_count))
@@ -36,48 +51,48 @@ class Github(GithubApi):
 			github_issue = Issue(**issue.raw_data)
 
 			if github_issue.milestone_id:
-
 				github_issue = AssociatedIssue(**issue.raw_data)
-
-				github_milestone = Milestone(**issue.raw_data["milestone"])
-
-				if github_milestone.id in [milestone.id for milestone in self.milestones]:
-					milestone = [milestone for milestone in self.milestones if milestone.id == github_milestone.id][0]
-				else:
-					milestone = github_milestone
-					self.milestones.append(milestone)
-
-				milestone.add_issue(github_issue)
-
+				milestone = Milestone(**issue.raw_data["milestone"])
+				self.add(milestone, "milestone")
 			else:
 				github_issue = SingleIssue(**issue.raw_data)
-				if github_issue.id in [issue.id for issue in self.single_issues]:
-					pass
-				else:
-					self.single_issues.append(github_issue)
+
+			self.add(github_issue, "issue")
+
+	def add(self, new_item, item_label):
+		"""
+		Add an item to a list
+		:param new_item: {object} - can be an Issue or a Milestone instance
+		 :param item_label: {str} - "milestone" | "issue"
+		:return: 
+		"""
+		if new_item.id not in [item.id for item in getattr(self, ITEM_LIST_MATCHING[item_label])]:
+			logging.info("Adding {} to {}".format(new_item, self))
+			getattr(self, ITEM_LIST_MATCHING[item_label]).add(new_item)
+		return new_item
 
 
-class Milestone(object):
+class Element(object):
 
 	def __init__(self, **kwargs):
 		self.id = kwargs.get("id")
 		self.title = kwargs.get("title")
 		self.description = kwargs.get("description")
 		self.state = kwargs.get("state")
+
+	def to_json_dict(self):
+		return self.__dict__
+
+	def __repr__(self):
+		return "{} <{} {}>".format(self.__class__.__name__, self.id, self.title)
+
+
+class Milestone(Element):
+
+	def __init__(self, **kwargs):
+		super(Milestone, self).__init__(**kwargs)
 		self.deadline = kwargs.get("deadline")
 		self.issues = set()
-
-	def get_list_index(self, lists=None):
-		if not lists: lists = []
-
-		if self.state == "closed": return len(lists) - 1
-		else:
-			if not self.issues: return 0
-			elif not self.deadline: return 0
-			else: return 1
-
-	def add_issue(self, issue):
-		self.issues.add(issue)
 
 	def has_closed_issues(self):
 		return len([issue for issue in list(self.issues) if issue.state == "closed"]) >= 1
@@ -85,34 +100,17 @@ class Milestone(object):
 	def has_open_issues(self):
 		return len([issue for issue in list(self.issues) if issue.state == "open"]) >= 1
 
-	def __repr__(self):
-		return "{} {}".format(self.__class__, self.title)
 
-
-class Issue(object):
+class Issue(Element):
 	def __init__(self, **kwargs):
-		self.id = kwargs.get("id")
-		self.title = kwargs.get("title")
-		self.description = kwargs.get("description")
+		super(Issue, self).__init__(**kwargs)
 		self.milestone_id = kwargs.get("milestone", {}).get("id", None) if kwargs.get("milestone") else None
-		self.state = kwargs.get("state")
 		self.assignees = [assignee["login"] for assignee in kwargs.get("assignees", [])]
-
-	def __repr__(self):
-		return "{} {}".format(self.__class__, self.title)
 
 
 class SingleIssue(Issue):
 	def __init__(self, **kwargs):
 		super(SingleIssue, self).__init__(**kwargs)
-
-	def get_list_index(self, lists=None):
-		if not lists: lists = []
-
-		if self.state == "closed":
-			return len(lists) - 1
-		else:
-			return 0
 
 
 class AssociatedIssue(Issue):
---
+++ tests/test_github_integration.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+class GithubIntegrationUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_load(self):
+		pass
+
+
+class ElementUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_to_json_dict(self):
+		pass
+
+
+class MilestoneUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+
+class IssueUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+
+class SingleIssueUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+
+class AssociatedIssueUnitTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
