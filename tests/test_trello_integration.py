8ef8f15d31901eac5ff55e78085268b754aaba9a
Bencodev <benoit.cotte@essec.edu>
2017-03-26 21:06:22 +0200

    start trello unit tests

--- tests/test_trello_integration.py
+++ tests/test_trello_integration.py
@@ -1,10 +1,98 @@
+# std
 import unittest
 
-
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, False)
+# project
+from mock_trello import *
 
 
-if __name__ == '__main__':
-	unittest.main()
+class TrelloUnitTest(unittest.TestCase):
+	pass
+
+
+class BoardUnitTest(unittest.TestCase):
+	def setUp(self):
+		from trello_integration import Board
+		self.board = Board(mock_trello_api, **{
+			"id": u"58ac046faad15d151e3b3623",  # Single Issues Board
+			"name": u"Test name"
+		})
+
+	def test_get_card(self):
+		"""
+		Expect proper card to be returned
+		:return:
+		"""
+		from trello_integration import Card
+		from github_integration import SingleIssue
+		card = Card.from_github(mock_trello_api, SingleIssue(**{
+			"id": u"test_id",
+			"title": u'Pusher / le118000 / street_number',
+			"description": u"Lorem Ipsum",
+			"state": u"open"
+		}))
+		self.assertEquals(self.board.get_card(card.name).name, card.name)
+		self.assertEquals(self.board.get_card("Card does not exist"), None)
+
+	def test_add_card(self):
+		"""
+		Expect card to be added to the board
+		:return:
+		"""
+		from trello_integration import Card
+		from github_integration import SingleIssue
+		card = Card.from_github(mock_trello_api, SingleIssue(**{
+			"id": u"test_id",
+			"title": u'Pusher / le118000 / street_number',
+			"description": u"Lorem Ipsum",
+			"state": u"open"
+		}))
+		self.assertEquals(type(self.board.add_card(card)), Card)
+
+	def test_update_card(self):
+		"""
+		Expect proper card to be added
+		:return:
+		"""
+		card = self.board.get_card(u'Pusher / le118000 / street_number')
+
+		self.assertEquals(self.board.update_card(card, {
+			"desc": "New description"
+		}), None)
+
+	def test_get_checklist_item(self):
+		from trello_integration import CheckListItem
+		from github_integration import AssociatedIssue
+		checklist_item = CheckListItem.from_github(mock_trello_api, AssociatedIssue(**{
+			"id": u"test_id",
+			"title": u'Pusher / le118000 / street_number',
+			"description": u"Lorem Ipsum",
+			"state": u"open"
+		}))
+		# self.assertEquals(self.board.get_card(card.name).name, card.name)
+		# self.assertEquals(self.board.get_card("Card does not exist"), None)
+
+	def test_add_checklist_item(self):
+		pass
+
+
+class MilestoneBoardUnitTest(unittest.TestCase):
+	pass
+
+
+class SingleIssueBoardUnitTest(unittest.TestCase):
+	pass
+
+
+class ListUnitTest(unittest.TestCase):
+	pass
+
+
+class CardUnitTest(unittest.TestCase):
+	pass
+
+class CheckListUnitTest(unittest.TestCase):
+	pass
+
+
+class CheckListItemUnitTest(unittest.TestCase):
+	pass
